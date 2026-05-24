114432855de3d7425010d9d6a168df8427bf618e
Arjav Jain <arjavjain.jain1942@gmail.com>
2026-05-24 23:24:42 +0530

    feat: overhaul frontend UI with new magic-themed styles and particle animations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,23 @@ import Result from "./pages/Result";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/answer/:id" element={<Answer />} />
-      <Route path="/result/:id" element={<Result />} />
-      <Route path="/home" element={<Navigate to="/" replace />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <div className="app-container">
+      <div className="magic-particles">
+        <div className="magic-particle particle-1"></div>
+        <div className="magic-particle particle-2"></div>
+        <div className="magic-particle particle-snitch"></div>
+        <div className="magic-particle particle-3"></div>
+        <div className="magic-particle particle-4"></div>
+        <div className="magic-particle particle-5"></div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/answer/:id" element={<Answer />} />
+        <Route path="/result/:id" element={<Result />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
   );
 }
 
